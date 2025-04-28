9cc3c821cda63c6dce039403d01950915fb2a388
Kakajan <kakajantayjikov@gmail.com>
2025-04-28 15:24:30 +0500

    fix image bug

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
-// next.config.js
 module.exports = {
     images: {
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: '162.0.211.12',
-                port: '3001',
-                pathname: '/**',
+                protocol: 'https',
+                hostname: 'api.oguzforum.com',
+                port: '',
+                pathname: '/uploads/**',
             },
         ],
     },
